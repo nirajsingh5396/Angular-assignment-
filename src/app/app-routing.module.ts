1527b6dbba80a689649a4904e76249b7be6adaa7
niraj <niraj.kumar@plexcel.com>
2021-01-14 16:12:50 +0530

    Created lazy loaded module and navigate to path : assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'assignment' },
+  { path: 'assignment', loadChildren: () => import('./assignment/assignment.module').then(m => m.AssignmentModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
