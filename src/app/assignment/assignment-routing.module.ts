3e19d4b430cc57aeaaee9b8add9bd06ef14f5cb2
niraj <niraj.kumar@plexcel.com>
2021-01-14 16:14:13 +0530

    Setup route for AssignmentRoutingModule

---
+++ src/app/assignment/assignment-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AssignmentComponent } from './assignment/assignment.component';
+
+
+const routes: Routes = [
+    { path: '', component: AssignmentComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AssignmentRoutingModule { }
