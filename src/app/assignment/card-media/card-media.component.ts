aae0e50abd9f50b4db50ee0cd917a6ba73340836
niraj <niraj.kumar@plexcel.com>
2021-01-14 16:17:57 +0530

    Created card-media component and updated html code

---
+++ src/app/assignment/card-media/card-media.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-media',
+  templateUrl: './card-media.component.html',
+  styleUrls: ['./card-media.component.scss']
+})
+export class CardMediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
